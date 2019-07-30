01ac23701428c26eea801958da92505ac84fb40a
Simas <simonas.deimantavicius@mediapark.com>
2019-07-30 10:37:09 +0300

    setup Routes

--- src/components/PlaylistItem.js
+++ src/components/PlaylistItem.js
@@ -3,9 +3,6 @@ import StyledPlaylistItem from './styles/StyledPlaylistItem';
 
 const PlaylistItem = ({ video, active, played }) => (
   <StyledPlaylistItem active={active} played={played}>
-    <div className="wbn-player__video-nr">{video.num}</div>
-    <div className="wbn-player__video-name">{video.title}</div>
-    <div className="wbn-player__video-time">{video.duration}</div>
   </StyledPlaylistItem>
 );
 
--- src/components/containers/App.js
+++ src/components/containers/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import WbnPlayer from './WbnPlayer';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
 
 const App = () => (
-  <div>
-    <WbnPlayer />
-  </div>
+  <BrowserRouter>
+    <Switch>
+      <Route exact path="/" component={WbnPlayer} />
+      <Route exact path="/:activeVideo" component={WbnPlayer} />
+    </Switch>
+  </BrowserRouter>
 );
 
 export default App;
